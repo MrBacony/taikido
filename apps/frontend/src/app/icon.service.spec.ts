e9ccafa6af32704e3678bcb188d6802fe0b87e67
Gregor Speck <gregor.speck@bridging-it.de>
2022-03-21 14:39:22 +0100

    updated dependencies

--- apps/frontend/src/app/icon.service.spec.ts
+++ apps/frontend/src/app/icon.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { IconService } from './icon.service';
 
 describe('IconService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({ teardown: { destroyAfterEach: false } })
+  );
 
   it('should be created', () => {
     const service: IconService = TestBed.get(IconService);
